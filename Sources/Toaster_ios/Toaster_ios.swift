aec194cbad3cc76c6e2805740b0615f0bc40a3db
Julien Gourdet <julien.gourdet.ext@francetv.fr>
2022-11-16 11:29:43 +0100

    setup swift package

---
+++ Sources/Toaster_ios/Toaster_ios.swift
@@ -0,0 +1,6 @@
+public struct Toaster_ios {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/Toaster_iosTests/Toaster_iosTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Toaster_ios
+
+final class Toaster_iosTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Toaster_ios().text, "Hello, World!")
+    }
+}
